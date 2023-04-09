e7e1f21388533ee2cae1245af1a9a35376564c2d
margoshkari <68592214+margoshkari@users.noreply.github.com>
2023-04-09 20:23:44 +0300

    changing total money

--- CFC/components/Main.js
+++ CFC/components/Main.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { View, Text, TouchableWithoutFeedback, FlatList, Image, Dimensions } from "react-native";
+import { View, Text, TouchableWithoutFeedback, FlatList, Image, Dimensions, Modal, TextInput, Keyboard } from "react-native";
 import styles from "../styles/MainPage"
 import { VictoryPie} from "victory-native";
 import getImageComponent from "../resources/imageComponent";
@@ -9,6 +9,8 @@ const { width, height } = Dimensions.get('window');
 export default function Main(){
     const [transactionMoney, setTransactionMoney] = useState(0);
     const [totalMoney, setTotalMoney] = useState(1457);
+    const [value, setValue] = useState('');
+    const inputRef = useRef(null);
     const [graphicData, setGraphicData] = useState([
     { x: "food", y: 10, fill: "#64EBC2", id: 1, image: "food.js" },
     { x: "family", y: 90, fill: "#FE8664", id: 2, image: "family.js" },
@@ -20,12 +22,21 @@ export default function Main(){
     { x: "health", y: 30, fill: "#8CFF98", id: 8, image: "health.js" },
     { x: "health", y: 30, fill: "#8CFF98", id: 9, image: "health.js" },])
     const [combinedData, setCombinedData] = useState([]);
+    const [modalVisible, setModalVisible] = useState(false);
+
     useEffect(() =>{
         combine();
+    }, []);
+    useEffect(() => {
         sum();
-    }, [])
-    const combine = () =>{
-        const newData = graphicData.reduce((acc, cur) => {
+    }, [combinedData]);
+    useEffect(() => {
+        if (modalVisible) {
+          inputRef.current?.focus();
+        }
+      }, [modalVisible]);
+    const combine = async () =>{
+        const newData = await graphicData.reduce((acc, cur) => {
             const index = acc.findIndex(item => item.x === cur.x);
             if (index === -1) {
               acc.push({ x: cur.x, y: cur.y, fill: cur.fill, id: cur.id, image: cur.image });
@@ -36,18 +47,47 @@ export default function Main(){
           }, []);
           setCombinedData(newData);
     }
-   const sum = () =>{
-    const total = combinedData.reduce((acc, cur) => acc + cur.y, 0);
+   const sum = async () =>{
+    const total = await combinedData.reduce((acc, cur) => acc + cur.y, 0);
     setTransactionMoney(total);
    }
     return(
         <View style={styles.app}>
-            <View style={styles.header}>
-                <View style={{flexDirection: "row", alignItems: "center"}}>
-                    <BagDollar/>
-                    <Text style={[styles.periodText, {fontSize: 20}]}>Total:</Text>
-                    <Text style={styles.totalMoney}>${totalMoney}</Text>
+
+            <Modal
+            animationType='fade'
+            transparent={true}
+            visible={modalVisible}>
+            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+                    <View style={styles.pModal} >
+                        <View style={styles.sModal}>
+                            <TextInput 
+                            keyboardType="numeric" 
+                            ref={inputRef}
+                            style={styles.inputMoney}
+                            value={value} 
+                            onChangeText={(value) => setValue(value)}/>
+                            <TouchableWithoutFeedback
+                            style={{ padding: 10, backgroundColor: 'red', alignSelf: 'flex-end' }}
+                            onPress={() => {
+                                setModalVisible(false);
+                                setTotalMoney(value ? Number(value) : 0);
+                                }}>
+                                <Text style={{ color: 'white', fontSize: 20 }}>Save</Text>
+                            </TouchableWithoutFeedback>
+                    </View>
                 </View>
+            </TouchableWithoutFeedback>
+        </Modal>
+            <View style={styles.header}>
+                <TouchableWithoutFeedback onPressIn={() => setModalVisible(true)}>
+                    <View style={{flexDirection: "row", alignItems: "center"}}>
+                        <BagDollar/>
+                        <Text style={[styles.periodText, {fontSize: 20}]}>Total:</Text>
+                        <Text style={styles.totalMoney}>${totalMoney}</Text>
+                    </View>
+                </TouchableWithoutFeedback>
+
                 <View style={{flexDirection: "row", alignItems: "center", width: "90%", justifyContent: 'space-around'}}>
                     <TouchableWithoutFeedback>
                         <Text style={[styles.periodText, {fontSize: 21}]}>Expences</Text>
