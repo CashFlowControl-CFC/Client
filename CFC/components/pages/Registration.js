8b50f0fc6534304607f04a9df471a0d1313849c7
Witcher-MTM <phantomit228@gmail.com>
2023-05-19 14:37:01 +0300

    Update Registration.js

--- CFC/components/pages/Registration.js
+++ CFC/components/pages/Registration.js
@@ -5,10 +5,10 @@ import EmailInput from "../AuthComponents/EmailInput";
 import PasswordInput from "../AuthComponents/PasswordInput";
 import AuthHeader from "../AuthComponents/AuthHeader";
 import { FIREBASE_AUTH, auth } from "../../modules/FirebaseConfig";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 
-export default function Registration({navigation}){
+export default function Registration({ navigation }) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [repeatedPass, setRepeatedPass] = useState('');
@@ -19,64 +19,70 @@ export default function Registration({navigation}){
     const [isValidRepeatedPass, setIsValidRepeatedPass] = useState(null);
     const [isPressed, setIsPressed] = useState(false);
 
-    const [loading,setLoading] = useState(false)
+    const [loading, setLoading] = useState(false)
     const auth = FIREBASE_AUTH
 
-    const isValid = async () =>{
-        if(emailPattern.test(email)){
-            setIsValidEmail(true);
-        }
-        else{
+    const isValid = async () => {
+        setLoading(true)
+        try {
+            if (emailPattern.test(email) && passwordPattern.test(password) && password === repeatedPass) {
+                setIsValidEmail(true);
+                setIsValidPassword(true);
+                setIsValidRepeatedPass(true);
+                await createUserWithEmailAndPassword(auth, email, password)
+            }
+            else {
+                setIsValidEmail(false);
+                setIsValidPassword(false);
+                setIsValidRepeatedPass(false);
+            }
+        } catch (error) {
+            console.log("error", error.message)
             setIsValidEmail(false);
-        }
-        if(passwordPattern.test(password)){
-            setIsValidPassword(true);
-        }
-        else{
             setIsValidPassword(false);
-        }
-        if(password === repeatedPass){
-            setIsValidRepeatedPass(true);
-        }
-        else{
             setIsValidRepeatedPass(false);
         }
+        finally {
+            setLoading(false)
+        }
     }
-    return(
+    return (
         <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
             <View style={general.app}>
-                
-                       <AuthHeader navigation={navigation} title={'Registration'}/> 
-                        <View style={[general.content, {flexDirection: 'column'}]}>
-                            <EmailInput email={email} 
-                            setEmail={setEmail} 
-                            isValidEmail={isValidEmail === false ? isValidEmail : true}
-                            setIsValidEmail={setIsValidEmail}
-                            />
-                            <PasswordInput password={password} 
-                            setPassword={setPassword} 
-                            isValidPassword={isValidPassword === false ? isValidPassword : true}
-                            setIsValidPassword={setIsValidPassword}
-                            text={'Enter your password'}
-                            />
-                            <PasswordInput password={repeatedPass} 
-                            setPassword={setRepeatedPass} 
-                            isValidPassword={isValidRepeatedPass === false ? isValidRepeatedPass : true}
-                            setIsValidPassword={setIsValidRepeatedPass}
-                            text={'Repeat your password'}
-                            />
-                            <View style={{position: 'absolute', bottom: 25}}>
-                            <TouchableWithoutFeedback 
-                                            onPress={isValid} 
-                                            onPressIn={() => setIsPressed(true)} 
-                                            onPressOut={() => setIsPressed(false)}>
-                                    <View style={[general.addAuthBtn, {backgroundColor: !isPressed ? '#FECC7A' : '#fcbe53'}]}>
-                                        <Text style={general.addText}>Next</Text>
-                                    </View>
-                                </TouchableWithoutFeedback>
-                            </View>
-                        </View>
+
+                <AuthHeader navigation={navigation} title={'Registration'} />
+                <View style={[general.content, { flexDirection: 'column' }]}>
+                    <EmailInput email={email}
+                        setEmail={setEmail}
+                        isValidEmail={isValidEmail === false ? isValidEmail : true}
+                        setIsValidEmail={setIsValidEmail}
+                    />
+                    <PasswordInput password={password}
+                        setPassword={setPassword}
+                        isValidPassword={isValidPassword === false ? isValidPassword : true}
+                        setIsValidPassword={setIsValidPassword}
+                        text={'Enter your password'}
+                    />
+                    <PasswordInput password={repeatedPass}
+                        setPassword={setRepeatedPass}
+                        isValidPassword={isValidRepeatedPass === false ? isValidRepeatedPass : true}
+                        setIsValidPassword={setIsValidRepeatedPass}
+                        text={'Repeat your password'}
+                    />
+                    <View style={{ position: 'absolute', bottom: 25 }}>
+                        {loading ? <ActivityIndicator style={general.addAuthBtn} size="large" color="#fcbe53" /> :
+                            <TouchableWithoutFeedback
+                                onPress={isValid}
+                                onPressIn={() => setIsPressed(true)}
+                                onPressOut={() => setIsPressed(false)}>
+                                <View style={[general.addAuthBtn, { backgroundColor: !isPressed ? '#FECC7A' : '#fcbe53' }]}>
+                                    <Text style={general.addText}>Next</Text>
+                                </View>
+                            </TouchableWithoutFeedback>
+                        }
                     </View>
+                </View>
+            </View>
         </TouchableWithoutFeedback>
     );
 }
