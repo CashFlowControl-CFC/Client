d1dcb9f5cdec144d09fdf59900e8338de0296b9a
margoshkari <68592214+margoshkari@users.noreply.github.com>
2023-05-29 18:25:49 +0300

    add target reducer

--- CFC/store/store.js
+++ CFC/store/store.js
@@ -3,13 +3,15 @@ import { transactionReducer } from './transactionReducer';
 import { categoriesReducer } from './categoriesReducer';
 import { transDataReducer } from './transDataReducer';
 import iconReducer from './iconReducer';
+import { targetReducer } from './targetReducer';
 
 
 const rootReducer = combineReducers({
     transaction: transactionReducer, 
     category: categoriesReducer,
     transData: transDataReducer,
-    icon: iconReducer
+    icon: iconReducer,
+    target: targetReducer
 })
 
 export default store = createStore(rootReducer);
---
+++ CFC/store/targetReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    targets: []
+}
+
+export const targetReducer = (state = initialState, action) => {
+    switch(action.type){
+        case 'SET_TARGETS':
+            return {...state, targets: action.payload};
+        case 'ADD_TARGET':
+            return {...state, targets: [...state.targets ,action.payload]};
+        default:
+            return state;
+    }
+}
