147654769eeaa11a7a5f6869152a9abf25ee4418
margoshkari <68592214+margoshkari@users.noreply.github.com>
2023-06-12 19:33:36 +0300

    upd

--- CFC/components/AuthComponents/PasswordInput.js
+++ CFC/components/AuthComponents/PasswordInput.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, TextInput, TouchableWithoutFeedback } from "react-native";
+import { View, Text, TextInput, TouchableWithoutFeedback, Platform } from "react-native";
 import general from "../../styles/general";
 import getImage from "../../resources/imageComponent";
 
@@ -25,9 +25,15 @@ export default function PasswordInput(props){
                     }}
                 />
                 <TouchableWithoutFeedback onPress={() => setIsClose(!isClose)}>
-                    <View style={{fontSize: 20, position: 'absolute', left: 10, bottom: 10}}>
+                    {Platform.OS == 'ios' ?  <View style={{fontSize: 20, position: 'absolute', left: 10, bottom: 10}}>
                         {getImage( isClose ? process.env.API_CLOSE_URL : process.env.API_OPEN_URL, 20, 20, '#FFFFFF')}
                     </View>
+                    :
+                    <View style={{fontSize: 20, position: 'absolute', right: 10, bottom: 10}}>
+                        {getImage( isClose ? process.env.API_CLOSE_URL : process.env.API_OPEN_URL, 20, 20, '#FFFFFF')}
+                    </View>
+                }
+                   
                 </TouchableWithoutFeedback>
             </View>
         </View>
